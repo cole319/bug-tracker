d963c8995e8cd5db75607c7b0693445e0827c5db
Suryarghya Saha <20je0998@cse.iitism.ac.in>
2025-08-11 12:05:22 +0530

    logout button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React from "react";
+import { useState } from "react";
 import Image from "next/image";
-import { IoMdMoon, IoMdSunny } from "react-icons/io";
+import { IoIosLogOut, IoMdMoon, IoMdSunny } from "react-icons/io";
 import { toggleTheme } from "@/features/theme/themeSlice";
 import { useAppDispatch, useAppSelector } from "@/stores/storeHooks";
 import useTheme from "@/hooks/useTheme";
@@ -10,6 +11,8 @@ export default function Header() {
   useTheme();
   const theme = useAppSelector((state) => state.theme.mode);
   const dispatch = useAppDispatch();
+
+  const [profileMenuOpen, setProfileMenuOpen] = useState<boolean>(false);
 
   return (
     <section className="w-full py-[1rem] md:pt-[3rem] md:pb-[1rem] px-[1rem] md:px-[2rem] lg:px-[4rem] flex justify-between items-center bg-bg-base dark:bg-d-bg-base">
@@ -27,14 +30,25 @@ export default function Header() {
         >
           {theme === "light" ? <IoMdMoon /> : <IoMdSunny />}
         </button>
+        <div className="relative">
+          <button onClick={() => setProfileMenuOpen(!profileMenuOpen)}>
+            <Image
+              src="/user.png"
+              height={40}
+              width={40}
+              alt="user icon"
+              className="rounded-full cursor-pointer"
+            />
+          </button>
 
-        <Image
-          src="/user.png"
-          height={40}
-          width={40}
-          alt="user icon"
-          className="rounded-full"
-        />
+          {profileMenuOpen && (
+            <div className="absolute bg-card-bg dark:bg-d-card-bg border-[0.2px] border-accent-primary/20 dark:border-d-accent-primary/20 p-[1rem] right-[1rem] z-50 rounded-md">
+              <button className="bg-accent-primary dark:bg-d-text-primary text-d-text-primary dark:text-text-primary font-medium text-[1rem] flex gap-[1rem] justify-center items-center px-[1rem] py-[0.5rem] cursor-pointer rounded-md">
+                Logout <IoIosLogOut />
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
